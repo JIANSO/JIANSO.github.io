7bd0efc9baedd054494977e23631ce550c6836ce
sojian <dothewillof@naver.com>
2024-08-20 15:21:30 +0900

    'update'

---
+++ assets/js/calendar.js
@@ -0,0 +1,17 @@
+let calender = {
+    create_table : function(days){
+
+        let week = `<tr>
+                <td></td>
+            </tr>`;
+
+        let html = `<table>
+        <thead></thead>
+        <tbody>
+            <tr></tr>
+        </tbody>
+        </table>`
+
+        return html
+    }
+}
